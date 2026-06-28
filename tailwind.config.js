eb53307e22f19daa1fa13970dd603f1070c82e5a
lrbear <hasandogruyol81@gmail.com>
2026-06-28 22:55:14 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7c3aed",
+        dark: "#0f0f17",
+      },
+    },
+  },
+  plugins: [],
+};
